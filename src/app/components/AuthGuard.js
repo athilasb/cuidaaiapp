7efb2dc1a660ad4d295225e194c9b6231b0367cd
athilasb <athila-sb@hotmail.com.br>
2025-05-16 16:30:57 -0300

    feat

--- src/app/components/AuthGuard.js
+++ src/app/components/AuthGuard.js
@@ -20,6 +20,7 @@ export default function AuthGuard({ children }) {
       const sessaoValida = result?.login.success;
 
       if (!sessaoValida) {
+        Cookies.remove('auth_token');
         router.push('/login');
       }
     };
