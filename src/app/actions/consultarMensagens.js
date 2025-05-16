f37d9a5a021edb028fdd5b2537c224158776f6ba
athilasb <athila-sb@hotmail.com.br>
2025-05-16 12:26:00 -0300

    feat ia

---
+++ src/app/actions/consultarMensagens.js
@@ -0,0 +1,13 @@
+'use server'
+export async function consultarMensagens(email, senha) {
+  const formData = new FormData();
+  formData.append('email', email);
+  formData.append('senha', senha);
+  const response = await fetch('https://infoadmdev.infodental.dental/infoservices/cuidaai/login.php', {
+    method: 'POST',
+    body: formData
+  });
+
+  const data = await response.json();
+  return data;
+}
